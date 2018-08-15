aaacf7f2629276edda2ecefa11b3e1dd6ae2ad9f
Vishal Gavali <vihisharal826@gmail.com>
2018-08-15 07:06:37 +0530

    added function object challenge start 005-js-object-function

---
+++ 005_JS_Object_Functions/challenge/script.js
@@ -0,0 +1,15 @@
+function print(x){
+  console.log(x);  
+} 
+function topictitle(x){
+    console.log("\n\n>>>>>>>>>>"+x+"<<<<<<<<<<<\n\n");
+}
+
+function subtopictitle(x){
+    console.log("\n    >>>"+x+"<<<\n");
+}
+
+function end(){             print('/*++++++++++++++++++++++++++++++++++++++++*/');
+}
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++*/
